7d3f09b17f5fe8a69bf559ecb5bcf5ec94fbd395
ANRgskyo <RAngskyo@gmail.com>
2019-01-16 08:55:56 +0000

    add viwes

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
 
+  attachment :image
+
   has_many :shopping_carts, through: :cart_items, dependent: :destroy
   has_many :songs, dependent: :destroy
   belongs_to :artist
