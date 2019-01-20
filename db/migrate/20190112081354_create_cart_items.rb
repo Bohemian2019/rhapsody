71ee00ea5c359bd3a501659d2406c7116c5e8e64
IKE054 <upperlake.k@gmail.com>
2019-01-20 05:21:57 +0000

    add foreign key to cart_items

--- db/migrate/20190112081354_create_cart_items.rb
+++ db/migrate/20190112081354_create_cart_items.rb
@@ -2,8 +2,12 @@ class CreateCartItems < ActiveRecord::Migration[5.2]
   def change
     create_table :cart_items do |t|
 
-      t.integer :shopping_cart_id, null: false
-      t.integer :item_id, null: false
+      # modified 20190120 add foreign key----------
+      # t.integer :shopping_cart_id, null: false
+      # t.integer :item_id, null: false
+      t.references :item, index: true, null: false
+      t.references :shopping_cart, index: true, null: false
+      # modified 20190120 -------------------------
       t.integer :price
       t. integer :quantity, null: false, default: 1
       t.timestamps
