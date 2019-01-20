e1c89d467a2186a66f2dd469104cf8ab860828a3
ANRgskyo <RAngskyo@gmail.com>
2019-01-20 04:13:42 +0000

    fix-views

--- app/controllers/community/questions_controller.rb
+++ app/controllers/community/questions_controller.rb
@@ -9,8 +9,7 @@ class Community::QuestionsController < ApplicationController
 
   def add
     @question = Question.new(question_params)
-    # 　後でcrrent_user.idに変更
-    @question.user_id = 1
+    @question.user_id = current_user.id
     @question.community_id = params[:id]
     @question.save
     redirect_to community_qa_show_path(params[:id], @question.id)
--- app/controllers/shopping_cart/orders_controller.rb
+++ app/controllers/shopping_cart/orders_controller.rb
@@ -1,4 +1,10 @@
 class ShoppingCart::OrdersController < ApplicationController
+
   def new
+  	@order = Order.find(params[:id])
+  	@shoppingcarts = ShoppingCart.all
+  	@items = @shoppingcarts.user_id
+  	
   end
+
 end
