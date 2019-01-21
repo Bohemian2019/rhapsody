c9876512e3b3ad1cddbf01d453478220fe3859cd
Shuichi <buzzplusmoney@gmail.com>
2019-01-21 09:55:19 +0000

    change

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,9 +4,16 @@ class Admin::OrdersController < ApplicationController
 
   def search
     @searched = Order.includes({:shopping_cart => {:items => :artist}},:shopping_cart =>  :user).page(params[:page])
-    #@searched = Order.search(params[:keyword_item]).page(params[:page])
+    @searched = Order.search(params[:keyword_item]).page(params[:page])
   end
 
   def show
+  	@order = Order.find(params[:id])
+  	@cart = CartItem.where(shopping_cart_id: @order.shopping_cart_id).first
+  	@shopping = ShoppingCart.where(id: @order.shopping_cart_id).first
+  	@item = Item.where(id: @cart.item_id)
+  	@user = User.where(id: @shopping.user_id).first
+    @credit = CreditCard.where(user_id: @user.id).first
+    @total = @cart.price * @cart.quantity
   end
 end
--- app/controllers/user/histories_controller.rb
+++ app/controllers/user/histories_controller.rb
@@ -1,8 +1,12 @@
 class User::HistoriesController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
   def all
+    @histories = Order.includes(:shopping_cart => :user).where(users: {id: current_user.id}).page(params[:page])
   end
 
   def index
+    @order = Order.find(params[:id])
+    @history = ShoppingCart.includes(:items => :artist).where(:id => (params[:id])).page(params[:page])
   end
 
   def show
