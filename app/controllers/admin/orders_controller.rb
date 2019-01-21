b85ec0705c818c421866fe4f47ba1a8ba232aa59
Shuichi <buzzplusmoney@gmail.com>
2019-01-21 13:32:44 +0000

    admin

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -8,11 +8,11 @@ class Admin::OrdersController < ApplicationController
   end
 
   def show
-  	@order = Order.find(params[:id])
-  	@cart = CartItem.where(shopping_cart_id: @order.shopping_cart_id).first
-  	@shopping = ShoppingCart.where(id: @order.shopping_cart_id).first
-  	@item = Item.where(id: @cart.item_id)
-  	@user = User.where(id: @shopping.user_id).first
+    @order = Order.find(params[:id])
+    @cart = CartItem.where(shopping_cart_id: @order.shopping_cart_id).first
+    @shopping = ShoppingCart.where(id: @order.shopping_cart_id).first
+    @item = Item.where(id: @cart.item_id)
+    @user = User.where(id: @shopping.user_id).first
     @credit = CreditCard.where(user_id: @user.id).first
     @total = @cart.price * @cart.quantity
   end
