091d3ab568f4d126ac7eac6944710b007890e6d4
Yota Ito <y0hta335@gmail.com>
2019-01-21 06:27:39 +0000

    Fix conflicts

--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -4,5 +4,5 @@ class ShoppingCart < ApplicationRecord
   has_one :order
   has_many :cart_items
   has_many :items, through: :cart_items, dependent: :destroy
-
+  # has_many :cart_items # 追加 'Fixed Association'　20190118
 end
