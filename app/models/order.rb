38b0215e605c08b695e99a4cf7aef3cfe4c48a08
Yota Ito <y0hta335@gmail.com>
2019-01-26 05:10:51 +0000

    Feature admin orders search

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,10 +3,10 @@ class Order < ApplicationRecord
   belongs_to :shopping_cart
 
   # Search method
-  def self.search(keyword)
-    if keyword
-      relation = Order.includes({:shopping_cart => {:items => :artist}}, :shopping_cart => :user)
-      relation.where(items: {item_name: "lalala"})
+  def self.search(keyword_artist, keyword_item)
+    if keyword_artist or keyword_item
+      relation = Order.joins({:shopping_cart => {:items => :artist}}, :shopping_cart => :user)
+      relation.merge(Artist.where("artist_name LIKE '%#{keyword_artist}%'")).merge(Item.where("item_name LIKE '%#{keyword_item}%'"))
     else
       all
     end
