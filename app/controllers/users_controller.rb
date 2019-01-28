a4f74c23d4b446d41de534c56954f5a833e22b70
IKE054 <upperlake.k@gmail.com>
2019-01-28 09:56:22 +0000

    mod users show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,32 +3,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    # 購入履歴リスト 新しいものから５件表示
-    @history = ShoppingCart.joins(:cart_items => :item)
-                          .select("cart_items.id", "items.item_name","items.artist_id")
-                          .where(:user_id => @user.id)
-                          .order("cart_items.id DESC")
-                          .limit(5)
-                          # 実行結果：
-                          # SELECT cart_items.id, items.item_name, items.artist_id
-                          # FROM "shopping_carts"
-                          # INNER JOIN "cart_items" ON "cart_items"."shopping_cart_id" = "shopping_carts"."id"
-                          # INNER JOIN "items" ON "items"."id" = "cart_items"."item_id"
-                          # WHERE "shopping_carts"."user_id" = $1
-                          # ORDER BY cart_items.id DESC
-                          # LIMIT $2  [["user_id", 1], ["LIMIT", 5]]
-    # マイコミュニティリスト 新らしいものから５件表示
-    @my_communities = UsersCommunity.joins(:community)
-                                      .select("communities.artist_id")
+    # 購入履歴リスト
+    @history = ShoppingCart.includes(:cart_items => {:item => :artist}).where(:user_id => @user.id).where(:is_active => false)
+    # マイコミュニティリスト
+    @my_communities = UsersCommunity.includes(:community => :artist)
                                       .where(:user_id => @user.id)
-                                      .order("users_communities.id DESC")
-                                      .limit(5)
-                                      # SELECT communities.artist_id
-                                      # FROM "users_communities"
-                                      # INNER JOIN "communities" ON "communities"."id" = "users_communities"."community_id"
-                                      # WHERE "users_communities"."user_id" = 1
-                                      # ORDER BY users_communities.id DESC
-    @artist = Artist
   end
 
   def edit
