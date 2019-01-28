2a1b1fabd342f32c89ee342684262ddb07603b46
IKE054 <upperlake.k@gmail.com>
2019-01-28 04:04:23 +0000

    add new community

--- app/controllers/admin/communities_controller.rb
+++ app/controllers/admin/communities_controller.rb
@@ -1,6 +1,26 @@
 class Admin::CommunitiesController < ApplicationController
   def new
+    @community = Community.where(id: params[:id])
+  end
 
+  def update
+    community = Community.find(params[:id])
+    count = Community.where(artist_id: community.artist_id).where(is_active: true).count
+    # communityをアーティストで検索、②以上であればエラー
+    if count >= 2
+      flash[:notice] = "当該アーティストにはすでにコミュニティが存在します"
+      @community = Community.where(id: params[:id])
+      render :new
+    else
+      if community.update!(community_params)
+        community.update(is_active: true)
+        redirect_to admin_communities_request_index_path
+        flash[:notice] = "コミュニティの新規登録が完了しました"
+      else
+        @community = Community.where(id: params[:id])
+        render :new
+      end
+    end
   end
 
   def request_index
@@ -12,4 +32,9 @@ class Admin::CommunitiesController < ApplicationController
     community.destroy
     redirect_to admin_communities_request_index_path
   end
+
+  def community_params
+    params.require(:community).permit(:name, :introduction)
+  end
+
 end
